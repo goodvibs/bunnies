2ba138696d12d7bd91e565ac4c4196d8ec1b767d
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2025-03-10 19:01:01 -0700

    Made all modules pub

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,14 @@ pub mod attacks;
 pub mod r#move;
 pub mod pgn;
 pub mod state;
-mod square;
-mod color;
-mod piece_type;
-mod colored_piece;
-mod bitboard;
+pub mod square;
+pub mod color;
+pub mod piece_type;
+pub mod colored_piece;
+pub mod bitboard;
 pub mod charboard;
 pub mod masks;
-mod move_direction;
+pub mod move_direction;
 
 #[cfg(test)]
 mod tests {}
