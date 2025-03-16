b90045f22a89d63adb7addc44c2e0002f54b0461
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2025-03-16 16:36:49 -0700

    Improvements

--- src/pgn/parsing.rs
+++ src/pgn/parsing.rs
@@ -22,30 +22,86 @@ pub enum PgnParseState {
     ResultFound
 }
 
+#[derive(Clone)]
 pub struct EnrichedMoveTreeNode {
     pub node: Rc<RefCell<MoveTreeNode>>,
     pub state_after_move: State,
 }
 
+#[derive(Clone)]
+pub struct Context {
+    pub current: EnrichedMoveTreeNode,
+    pub previous: Option<EnrichedMoveTreeNode>,
+}
+
+impl Context {
+    pub fn next(&mut self, new_node: &Rc<RefCell<MoveTreeNode>>, new_state: State) {
+        self.current.node.borrow_mut().add_continuation(new_node);
+
+        // Create the new value we want to assign to self.current
+        let new_current = EnrichedMoveTreeNode {
+            node: Rc::clone(new_node),
+            state_after_move: new_state,
+        };
+
+        // Replace self.current with new_current and get the old value
+        let old_current = std::mem::replace(&mut self.current, new_current);
+
+        // Set previous to the old current value
+        self.previous = Some(old_current);
+    }
+}
+
+pub struct ContextManager {
+    pub context: Context,
+    pub stack: Vec<Context>,
+}
+
+impl ContextManager {
+    pub fn new(root_node: &Rc<RefCell<MoveTreeNode>>, initial_state: State) -> ContextManager {
+        ContextManager {
+            context: Context {
+                current: EnrichedMoveTreeNode {
+                    node: Rc::clone(root_node),
+                    state_after_move: initial_state,
+                },
+                previous: None,
+            },
+            stack: Vec::new(),
+        }
+    }
+
+    pub fn create_branch_from_previous(&mut self) {
+        let clone_of_previous = self.context.previous.clone().expect("No previous node to create branch from");
+        let new_context = Context {
+            current: clone_of_previous,
+            previous: None,
+        };
+        let old_context = std::mem::replace(&mut self.context, new_context);
+        self.stack.push(old_context);
+    }
+
+    pub fn end_branch(&mut self) {
+        let previous_context = self.stack.pop().expect("No previous context to return to");
+        self.context = previous_context;
+    }
+}
+
 pub struct PgnParser {
     pub parse_state: PgnParseState,
-    pub move_tree: PgnObject,
-    pub current: EnrichedMoveTreeNode,
-    pub stack: Vec<EnrichedMoveTreeNode>,
+    pub object: PgnObject,
+    pub context_manager: ContextManager
 }
 
 impl PgnParser {
     pub fn new() -> PgnParser {
-        let move_tree = PgnObject::new();
-        let current_node = Rc::clone(&move_tree.tree_root);
+        let pgn_object = PgnObject::new();
+        let current_node = &pgn_object.tree_root;
+        let context_manager = ContextManager::new(&current_node, State::initial());
         PgnParser {
             parse_state: PgnParseState::Tags,
-            move_tree,
-            current: EnrichedMoveTreeNode {
-                node: current_node,
-                state_after_move: State::initial(),
-            },
-            stack: Vec::new(),
+            object: pgn_object,
+            context_manager,
         }
     }
 
@@ -70,10 +126,10 @@ impl PgnParser {
                     self.process_move::<PgnCastlingMove>(mv)?;
                 }
                 PgnToken::StartVariation => {
-                    // self.process_start_variation()?;
+                    self.process_start_variation()?;
                 }
                 PgnToken::EndVariation => {
-                    // self.process_end_variation()?;
+                    self.process_end_variation()?;
                 }
                 PgnToken::Comment(comment) => {
                     // self.process_comment(comment)?;
@@ -93,7 +149,7 @@ impl PgnParser {
         if self.parse_state != PgnParseState::Tags {
             return Err(PgnParseError::UnexpectedToken(format!("Unexpected tag token: {:?}", tag)));
         }
-        self.move_tree.add_tag(tag.name, tag.value);
+        self.object.add_tag(tag.name, tag.value);
         Ok(())
     }
 
@@ -108,7 +164,7 @@ impl PgnParser {
                     Err(PgnParseError::UnexpectedToken(format!("Unexpected move number token: {:?}", pgn_move_number)))
                 }
                 else {
-                    let expected_fullmove = self.current.state_after_move.get_fullmove();
+                    let expected_fullmove = self.context_manager.context.current.state_after_move.get_fullmove();
                     if pgn_move_number.fullmove_number == expected_fullmove {
                         self.parse_state = PgnParseState::Moves { move_number_just_seen: true };
                         Ok(())
@@ -126,7 +182,7 @@ impl PgnParser {
     fn process_move<PgnMoveType: PgnMove>(&mut self, pgn_move: PgnMoveType) -> Result<(), PgnParseError> {
         match self.parse_state {
             PgnParseState::Moves { move_number_just_seen } => {
-                let mut current_state = &self.current.state_after_move;
+                let mut current_state = &self.context_manager.context.current.state_after_move;
                 if !(move_number_just_seen || current_state.side_to_move == Color::Black) {
                     return Err(PgnParseError::UnexpectedToken(format!("Unexpected move token: {:?}", pgn_move)));
                 }
@@ -155,11 +211,7 @@ impl PgnParser {
                         nag: pgn_move.get_common_move_info().nag.clone(),
                     };
                     let new_node = Rc::new(RefCell::new(MoveTreeNode::new(move_data, None)));
-                    self.current.node.borrow_mut().add_continuation(&new_node);
-                    self.current = EnrichedMoveTreeNode {
-                        node: new_node,
-                        state_after_move: new_state,
-                    };
+                    self.context_manager.context.next(&new_node, new_state);
                     self.parse_state = PgnParseState::Moves { move_number_just_seen: false };
                     Ok(())
                 } else {
@@ -168,6 +220,34 @@ impl PgnParser {
             }
             _ => {
                 Err(PgnParseError::UnexpectedToken(format!("Unexpected move token: {:?}", pgn_move)))
+            }
+        }
+    }
+
+    fn process_start_variation(&mut self) -> Result<(), PgnParseError> {
+        match self.parse_state {
+            PgnParseState::Moves { move_number_just_seen: false } => {
+                self.context_manager.create_branch_from_previous();
+                Ok(())
+            }
+            _ => {
+                Err(PgnParseError::UnexpectedToken("Unexpected start variation token".to_string()))
+            }
+        }
+    }
+
+    fn process_end_variation(&mut self) -> Result<(), PgnParseError> {
+        match self.parse_state {
+            PgnParseState::Moves { move_number_just_seen: false } => {
+                if self.context_manager.stack.is_empty() {
+                    Err(PgnParseError::UnexpectedToken("Unexpected end variation token".to_string()))
+                } else {
+                    self.context_manager.end_branch();
+                    Ok(())
+                }
+            }
+            _ => {
+                Err(PgnParseError::UnexpectedToken("Unexpected end variation token".to_string()))
             }
         }
     }
