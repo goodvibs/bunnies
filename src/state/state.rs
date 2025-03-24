1bc3a0c60bff13581d546d6c55a0185d04e51a54
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2025-03-23 18:40:15 -0700

    Added unit tests

--- src/state/state.rs
+++ src/state/state.rs
@@ -78,3 +78,82 @@ impl State {
         }
     }
 }
+
+#[cfg(test)]
+mod state_tests {
+    use std::cell::RefCell;
+    use std::rc::Rc;
+    use crate::utils::{Color, ColoredPiece, PieceType, Square};
+    use crate::state::{GameContext, GameResult, State};
+    use crate::utils::charboard::print_bb;
+
+    #[test]
+    fn test_initial_state() {
+        let state = State::initial();
+        assert_eq!(state.side_to_move, Color::White);
+        assert_eq!(state.halfmove, 0);
+        assert_eq!(state.result, GameResult::None);
+        assert_eq!(state.get_fullmove(), 1);
+    }
+
+    #[test]
+    fn test_get_fullmove() {
+        let mut state = State::initial();
+
+        assert_eq!(state.get_fullmove(), 1); // Initial position
+
+        state.halfmove = 1;
+        assert_eq!(state.get_fullmove(), 1); // After 1 halfmove
+
+        state.halfmove = 2;
+        assert_eq!(state.get_fullmove(), 2); // After 2 halfmoves
+
+        state.halfmove = 3;
+        assert_eq!(state.get_fullmove(), 2); // After 3 halfmoves
+
+        state.halfmove = 10;
+        assert_eq!(state.get_fullmove(), 6); // After 10 halfmoves
+    }
+
+    #[test]
+    fn test_current_side_attacks() {
+        let state = State::initial();
+        let expected_attacks = state.board.calc_attacks_mask(state.side_to_move);
+        print_bb(expected_attacks);
+        assert_eq!(state.current_side_attacks(), expected_attacks);
+    }
+
+    #[test]
+    fn test_opposite_side_attacks() {
+        let initial_state = State::initial();
+        let initial_black_attacks = initial_state.board.calc_attacks_mask(initial_state.side_to_move.flip());
+        assert_eq!(initial_state.opposite_side_attacks(), initial_black_attacks);
+        initial_state.context.borrow_mut().initialize_current_side_attacks(initial_black_attacks);
+        
+        let mut next_state_board = initial_state.board.clone();
+        next_state_board.move_colored_piece(
+            ColoredPiece::from(Color::White, PieceType::Pawn),
+            Square::E4,
+            Square::E2
+        );
+        let next_state_zobrist = next_state_board.zobrist_hash;
+        
+        
+        let next_state_context = GameContext::new_with_previous(
+            &initial_state.context,
+            next_state_zobrist,
+            0
+        );
+        
+        let next_state = State {
+            board: next_state_board,
+            side_to_move: Color::Black,
+            halfmove: 1,
+            result: GameResult::None,
+            context: Rc::new(RefCell::new(next_state_context)),
+        };
+        
+        let next_state_white_attacks = next_state.board.calc_attacks_mask(next_state.side_to_move.flip());
+        assert_eq!(next_state.opposite_side_attacks(), next_state_white_attacks);
+    }
+}
