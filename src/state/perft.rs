2da3d0ce8de832df9208a03a3142d0d2f416687d
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2025-03-24 11:32:35 -0700

    Looks nicer

--- src/state/perft.rs
+++ src/state/perft.rs
@@ -2,24 +2,23 @@ use crate::state::State;
 
 pub fn count_nodes(state: &mut State, depth: u8) -> u64 {
     if !state.is_probably_valid() {
-        return 0;
+        0
+    } else if depth == 0 {
+        1
+    } else {
+        let mut total_nodes = 0;
+
+        let mut attacks = 0;
+        let pseudolegal_moves = state.calc_pseudolegal_moves(&mut attacks);
+
+        for mv in pseudolegal_moves {
+            state.make_move(mv, attacks);
+            total_nodes += count_nodes(state, depth - 1);
+            state.unmake_move(mv);
+        }
+
+        total_nodes
     }
-    if depth == 0 {
-        return 1;
-    }
-
-    let mut total_nodes = 0;
-
-    let mut attacks = 0;
-    let pseudolegal_moves = state.calc_pseudolegal_moves(&mut attacks);
-
-    for mv in pseudolegal_moves {
-        state.make_move(mv, attacks);
-        total_nodes += count_nodes(state, depth - 1);
-        state.unmake_move(mv);
-    }
-
-    total_nodes
 }
 
 impl State {
