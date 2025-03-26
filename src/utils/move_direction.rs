f0bea725c920ed1cd457a078355ad70339447544
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2025-03-26 16:35:01 -0700

    Added docs and renamed methods for consistency

--- src/utils/move_direction.rs
+++ src/utils/move_direction.rs
@@ -1,9 +1,8 @@
-//! Move direction related enums and functions.
-
 use crate::utils::Square;
 
 #[repr(u8)]
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
+/// Represents the direction of a Queen-like move (8 directions).
 pub enum QueenLikeMoveDirection {
     Up=0, Down=7,
     UpRight=1, DownLeft=6,
@@ -11,78 +10,63 @@ pub enum QueenLikeMoveDirection {
     DownRight=3, UpLeft=4,
 }
 
-const ALL_QUEEN_MOVE_DIRECTIONS: [QueenLikeMoveDirection; 8] = [
-    QueenLikeMoveDirection::Up, QueenLikeMoveDirection::Down,
-    QueenLikeMoveDirection::UpRight, QueenLikeMoveDirection::DownLeft,
-    QueenLikeMoveDirection::Right, QueenLikeMoveDirection::Left,
-    QueenLikeMoveDirection::DownRight, QueenLikeMoveDirection::UpLeft
-];
-
 impl QueenLikeMoveDirection {
-    pub const fn from(value: u8) -> QueenLikeMoveDirection {
+    /// An array of all QueenLikeMoveDirections (8 directions).
+    pub const ALL: [QueenLikeMoveDirection; 8] = [
+        QueenLikeMoveDirection::Up, QueenLikeMoveDirection::Down,
+        QueenLikeMoveDirection::UpRight, QueenLikeMoveDirection::DownLeft,
+        QueenLikeMoveDirection::Right, QueenLikeMoveDirection::Left,
+        QueenLikeMoveDirection::DownRight, QueenLikeMoveDirection::UpLeft
+    ];
+    
+    /// Returns the QueenLikeMoveDirection corresponding to the given value.
+    /// # Safety
+    /// The value must be in the range 0..=7.
+    pub const unsafe fn from(value: u8) -> QueenLikeMoveDirection {
         unsafe { std::mem::transmute::<u8, QueenLikeMoveDirection>(value) }
     }
     
-    pub const fn flip(&self) -> QueenLikeMoveDirection {
-        QueenLikeMoveDirection::from(7u8.wrapping_sub(*self as u8))
+    /// Returns the QueenLikeMoveDirection opposite to the current direction.
+    pub const fn opposite(&self) -> QueenLikeMoveDirection {
+        unsafe { QueenLikeMoveDirection::from(7u8.wrapping_sub(*self as u8)) }
     }
     
-    pub fn iter() -> impl Iterator<Item=QueenLikeMoveDirection> {
-        ALL_QUEEN_MOVE_DIRECTIONS.iter().copied()
-    }
-
-    pub const fn calc(src_square: Square, dst_square: Square) -> QueenLikeMoveDirection {
+    /// Returns a QueenLikeMoveDirection as calculated from the source and destination squares.
+    /// `distance_output` is set to the distance between the source and destination squares.
+    /// If the source and destination squares are not in the same line, the behavior is undefined.
+    pub const fn calc(src_square: Square, dst_square: Square, distance_output: &mut u8) -> QueenLikeMoveDirection {
         let value_change = dst_square as i8 - src_square as i8;
 
         let positive_direction;
+        let displacement;
 
         if value_change % 8 == 0 {
             positive_direction = QueenLikeMoveDirection::Down;
+            displacement = value_change / 8;
         } else if value_change % 9 == 0 {
             positive_direction = QueenLikeMoveDirection::DownRight;
+            displacement = value_change / 9;
         } else if src_square.rank() == dst_square.rank() {
             positive_direction = QueenLikeMoveDirection::Right;
+            displacement = value_change;
         } else {
             positive_direction = QueenLikeMoveDirection::DownLeft;
+            displacement = value_change / 7;
         }
 
         if value_change < 0 {
-            positive_direction.flip()
+            *distance_output = -displacement as u8;
+            positive_direction.opposite()
         } else {
+            *distance_output = displacement as u8;
             positive_direction
-        }
-    }
-
-    pub const fn calc_and_measure_distance(src_square: Square, dst_square: Square) -> (QueenLikeMoveDirection, u8) {
-        let value_change = dst_square as i8 - src_square as i8;
-
-        let positive_direction;
-        let distance_temp;
-
-        if value_change % 8 == 0 {
-            positive_direction = QueenLikeMoveDirection::Down;
-            distance_temp = value_change / 8;
-        } else if value_change % 9 == 0 {
-            positive_direction = QueenLikeMoveDirection::DownRight;
-            distance_temp = value_change / 9;
-        } else if src_square.rank() == dst_square.rank() {
-            positive_direction = QueenLikeMoveDirection::Right;
-            distance_temp = value_change;
-        } else {
-            positive_direction = QueenLikeMoveDirection::DownLeft;
-            distance_temp = value_change / 7;
-        }
-
-        if value_change < 0 {
-            (positive_direction.flip(), -distance_temp as u8)
-        } else {
-            (positive_direction, distance_temp as u8)
         }
     }
 }
 
 #[repr(u8)]
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
+/// Represents the direction of a Knight move (8 directions).
 pub enum KnightMoveDirection {
     TwoUpOneRight=0, TwoDownOneLeft=7,
     TwoRightOneUp=1, TwoLeftOneDown=6,
@@ -90,26 +74,29 @@ pub enum KnightMoveDirection {
     TwoDownOneRight=3, TwoUpOneLeft=4,
 }
 
-const ALL_KNIGHT_MOVE_DIRECTIONS: [KnightMoveDirection; 8] = [
-    KnightMoveDirection::TwoUpOneRight, KnightMoveDirection::TwoDownOneLeft,
-    KnightMoveDirection::TwoRightOneUp, KnightMoveDirection::TwoLeftOneDown,
-    KnightMoveDirection::TwoRightOneDown, KnightMoveDirection::TwoLeftOneUp,
-    KnightMoveDirection::TwoDownOneRight, KnightMoveDirection::TwoUpOneLeft
-];
-
 impl KnightMoveDirection {
-    pub const fn flip(&self) -> KnightMoveDirection {
+    /// An array of all KnightMoveDirections (8 directions).
+    pub const ALL: [KnightMoveDirection; 8] = [
+        KnightMoveDirection::TwoUpOneRight, KnightMoveDirection::TwoDownOneLeft,
+        KnightMoveDirection::TwoRightOneUp, KnightMoveDirection::TwoLeftOneDown,
+        KnightMoveDirection::TwoRightOneDown, KnightMoveDirection::TwoLeftOneUp,
+        KnightMoveDirection::TwoDownOneRight, KnightMoveDirection::TwoUpOneLeft
+    ];
+    
+    /// Returns the KnightMoveDirection opposite to the current direction.
+    pub const fn opposite(&self) -> KnightMoveDirection {
         KnightMoveDirection::from(7u8.wrapping_sub(*self as u8))
     }
 
+    /// Returns the KnightMoveDirection corresponding to the given value.
+    /// # Safety
+    /// The value must be in the range 0..=7.
     pub const fn from(value: u8) -> KnightMoveDirection {
         unsafe { std::mem::transmute::<u8, KnightMoveDirection>(value) }
     }
-    
-    pub fn iter() -> impl Iterator<Item=KnightMoveDirection> {
-        ALL_KNIGHT_MOVE_DIRECTIONS.iter().copied()
-    }
 
+    /// Returns a KnightMoveDirection as calculated from the source and destination squares.
+    /// If the source and destination squares are not a valid Knight move, the behavior is undefined.
     pub const fn calc(src_square: Square, dst_square: Square) -> KnightMoveDirection {
         let value_change = dst_square as i8 - src_square as i8;
 
@@ -126,7 +113,7 @@ impl KnightMoveDirection {
         }
 
         if value_change < 0 {
-            positive_direction.flip()
+            positive_direction.opposite()
         } else {
             positive_direction
         }
@@ -154,8 +141,9 @@ mod tests {
 
             if let Some(next_square) = next_square {
                 distance += 1;
-                assert_eq!(QueenLikeMoveDirection::calc(square, next_square), direction);
-                assert_eq!(QueenLikeMoveDirection::calc_and_measure_distance(square, next_square), (direction, distance));
+                let mut distance_found = 0;
+                assert_eq!(QueenLikeMoveDirection::calc(square, next_square, &mut distance_found), direction);
+                assert_eq!(distance, distance_found);
                 current_square = next_square;
             } else {
                 break;
@@ -164,7 +152,7 @@ mod tests {
     }
 
     fn test_all_queen_directions_for_square(square: Square) {
-        for direction in QueenLikeMoveDirection::iter() {
+        for direction in QueenLikeMoveDirection::ALL {
             test_queen_direction_for_square(square, direction);
         }
     }
@@ -194,7 +182,7 @@ mod tests {
     }
     
     fn test_all_knight_directions_for_square(square: Square) {
-        for direction in KnightMoveDirection::iter() {
+        for direction in KnightMoveDirection::ALL {
             test_knight_direction_for_square(square, direction);
         }
     }
