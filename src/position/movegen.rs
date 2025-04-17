c65a90a7d86c80b1369640633c727fe4f6274d3c
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2025-04-17 13:02:31 -0700

    Reduced redundant code

--- src/position/movegen.rs
+++ src/position/movegen.rs
@@ -20,8 +20,7 @@ impl Position {
         moves: &mut Vec<Move>,
         pawn_srcs: MaskBitsIterator,
     ) {
-        let opposite_color = self.side_to_move.other();
-        let opposite_color_bb = self.board.color_masks[opposite_color as usize];
+        let opposite_side_pieces = self.opposite_side_pieces();
 
         let promotion_rank = match self.side_to_move {
             Color::White => RANK_8,
@@ -33,7 +32,7 @@ impl Position {
 
             let pawn_attacks = multi_pawn_attacks(src, self.side_to_move);
 
-            let pawn_captures = pawn_attacks & opposite_color_bb;
+            let pawn_captures = pawn_attacks & opposite_side_pieces;
 
             for dst in pawn_captures.iter_set_bits_as_masks() {
                 let move_dst = unsafe { Square::from_bitboard(dst) };
@@ -52,8 +51,8 @@ impl Position {
 
     fn add_en_passant_pseudolegal(&self, moves: &mut Vec<Move>) {
         let context = unsafe { &*self.context };
-        let same_color_bb = self.board.color_masks[self.side_to_move as usize];
-        let pawns_bb = self.board.piece_type_masks[PieceType::Pawn as usize] & same_color_bb;
+        let current_side_pieces = self.current_side_pieces();
+        let current_side_pawns = self.board.pawns() & current_side_pieces;
 
         let (src_rank_bb, src_rank_first_square, dst_rank_first_square) = match self.side_to_move {
             Color::White => (RANK_5, Square::A5, Square::A6),
@@ -70,7 +69,7 @@ impl Position {
                     // if within bounds
                     let double_pawn_push_file_mask = FILE_A >> double_pawn_push_file;
 
-                    if pawns_bb & double_pawn_push_file_mask & src_rank_bb != 0 {
+                    if current_side_pawns & double_pawn_push_file_mask & src_rank_bb != 0 {
                         let move_src = unsafe {
                             Square::from(src_rank_first_square as u8 + double_pawn_push_file as u8)
                         };
@@ -92,7 +91,7 @@ impl Position {
     }
 
     fn add_pawn_push_pseudolegal(&self, moves: &mut Vec<Move>, pawn_srcs: MaskBitsIterator) {
-        let all_occupancy_bb = self.board.piece_type_masks[PieceType::ALL_PIECE_TYPES as usize];
+        let all_occupancy_bb = self.board.pieces();
 
         let promotion_rank = RANK_8 >> (self.side_to_move as u8 * 7 * 8); // RANK_8 for white, RANK_1 for black
 
@@ -206,7 +205,6 @@ impl Position {
         let king_src_square = unsafe { Square::from_bitboard(king_src_bb) };
 
         let king_attacks = single_king_attacks(king_src_square);
-
         let king_moves = king_attacks & !current_side_mask;
 
         for dst_square in king_moves.iter_set_bits_as_squares() {
