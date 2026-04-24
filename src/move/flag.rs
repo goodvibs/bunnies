e1d705e3e187e48c2b460f4c45f1ac847545f251
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2026-04-24 16:35:51 -0700

    temp

--- src/move/flag.rs
+++ src/move/flag.rs
@@ -1,34 +1,58 @@
+use crate::Piece;
+
 /// Enum representing the different types of moves that can be made in a game of chess.
 /// Used in the Move struct.
 #[repr(u8)]
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
-pub enum MoveFlag {
-    NormalMove = 0,
-    Promotion = 1,
-    EnPassant = 2,
-    Castling = 3,
+#[rustfmt::skip]
+pub enum MoveType {
+    Normal                   = 0b0000,
+    DoublePawnPush           = 0b0001,
+    Castling                 = 0b0010,
+
+    NormalCapture            = 0b0100,
+    EnPassant                = 0b0101,
+
+    PushPromotionToKnight    = 0b1000,
+    PushPromotionToBishop    = 0b1001,
+    PushPromotionToRook      = 0b1010,
+    PushPromotionToQueen     = 0b1011,
+
+    CapturePromotionToKnight = 0b1100,
+    CapturePromotionToBishop = 0b1101,
+    CapturePromotionToRook   = 0b1110,
+    CapturePromotionToQueen  = 0b1111,
 }
 
-impl MoveFlag {
-    /// Converts a u8 value to a MoveFlag.
-    pub const unsafe fn from(value: u8) -> MoveFlag {
-        debug_assert!(value < 4, "Invalid MoveFlag value");
-        unsafe { std::mem::transmute::<u8, MoveFlag>(value) }
+impl MoveType {
+    /// Converts a u8 value to a MoveType.
+    pub const unsafe fn from_u8(value: u8) -> MoveType {
+        debug_assert!(value < 4, "Invalid MoveType value");
+        unsafe { std::mem::transmute::<u8, MoveType>(value) }
     }
 
-    /// Returns a readable representation of the move flag.
-    pub const fn to_readable(&self) -> &str {
+    pub const fn is_capture(self) -> bool {
+        self as u8 & 0b0100 != 0
+    }
+
+    pub const fn is_promotion(self) -> bool {
+        self as u8 & 0b1000 != 0
+    }
+
+    pub const fn moved_piece(self) -> Piece {
         match self {
-            MoveFlag::NormalMove => "",
-            MoveFlag::Promotion => "[P to ?]",
-            MoveFlag::EnPassant => "[e.p.]",
-            MoveFlag::Castling => "[castling]",
+            Self::Normal => Piece::Null,
+            Self::Castling => Piece::King,
+            Self::NormalCapture => Piece::Null,
+            _ => Piece::Pawn,
         }
     }
-}
 
-impl From<u8> for MoveFlag {
-    fn from(value: u8) -> MoveFlag {
-        unsafe { MoveFlag::from(value) }
+    const fn promotion_bits(self) -> u8 {
+        self as u8 & 0b0011
+    }
+
+    pub const fn promotion_piece(self) -> Piece {
+        unsafe { Piece::from(self.promotion_bits() - Piece::Knight as u8) }
     }
 }
