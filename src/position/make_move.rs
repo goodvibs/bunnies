88d5a9f06d12d43ebe8ebd54bf2654ef66858220
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2026-04-27 23:17:40 -0700

    temp

--- src/position/make_move.rs
+++ src/position/make_move.rs
@@ -2,12 +2,14 @@
 
 use crate::Color;
 use crate::ColoredPiece;
+use crate::File;
 use crate::Flank;
 use crate::Piece;
+use crate::Rank;
 use crate::Square;
 use crate::r#move::{Move, MoveType};
-use crate::position::{GameResult, Position};
 use crate::position::context::PositionContext;
+use crate::position::{GameResult, Position};
 use crate::{ConstDoublePawnPushFile, DoublePawnPushFile};
 
 impl<const N: usize, const STM: Color> Position<N, STM> {
@@ -99,50 +101,48 @@ impl<const N: usize, const STM: Color> Position<N, STM> {
         let dst_square = mv.destination();
         let move_type = mv.move_type();
         let mover = STM.other();
+        let moved = self.board.piece_at(dst_square);
+
+        self.board
+            .remove_colored_piece_at(ColoredPiece::new(mover, moved), dst_square);
+
+        self.board
+            .put_colored_piece_at(ColoredPiece::new(mover, moved), src_square);
 
         match move_type {
             MoveType::Castling => {
-                debug_assert_eq!(self.context().captured_piece, Piece::Null);
-                self.board.apply_move::<false>(
-                    dst_square,
-                    src_square,
-                    mover,
-                    Piece::King,
-                    Piece::Null,
-                    move_type,
+                let flank = dst_square.file().flank();
+                let rook_from = castling_rook_from_square(flank, mover);
+                let rook_to = castling_rook_to_square(flank, mover);
+
+                self.board.move_colored_piece(
+                    ColoredPiece::new(mover, Piece::Rook),
+                    rook_from,
+                    rook_to,
                 );
             }
             MoveType::EnPassant => {
-                debug_assert_eq!(self.context().captured_piece, Piece::Pawn);
-                self.board.apply_move::<false>(
-                    dst_square,
-                    src_square,
-                    mover,
-                    Piece::Pawn,
-                    Piece::Null,
-                    move_type,
-                );
+                let capture_square =
+                    Square::from_rank_and_file(mover.en_passant_capture_rank(), dst_square.file());
+
+                self.board
+                    .put_colored_piece_at(ColoredPiece::new(mover, Piece::Pawn), capture_square);
             }
-            MoveType::Normal | MoveType::NormalCapture => {
-                self.board.apply_move::<false>(
-                    dst_square,
-                    src_square,
-                    mover,
-                    self.board.piece_at(dst_square),
-                    self.context().captured_piece,
-                    move_type,
-                );
-            }
-            _ => {
-                self.board.apply_move::<false>(
-                    dst_square,
-                    src_square,
-                    mover,
-                    Piece::Pawn,
-                    self.context().captured_piece,
-                    move_type,
-                );
-            }
+            _ => {}
+        }
+
+        if move_type.is_capture() && move_type != MoveType::EnPassant {
+            self.board.put_colored_piece_at(
+                ColoredPiece::new(STM, self.context().captured_piece),
+                dst_square,
+            );
+        }
+
+        if move_type.is_promotion() {
+            let promotion_piece = move_type.promotion_piece();
+
+            self.board
+                .remove_colored_piece_at(ColoredPiece::new(mover, promotion_piece), dst_square);
         }
 
         self.halfmove -= 1;
@@ -234,5 +234,21 @@ impl PositionContext {
                 _ => self.castling_rights,
             };
         }
+    }
+}
+
+const fn castling_rook_from_square(flank: Flank, color: Color) -> Square {
+    let rank = Rank::One.from_perspective(color);
+    match flank {
+        Flank::Kingside => Square::from_rank_and_file(rank, File::H),
+        Flank::Queenside => Square::from_rank_and_file(rank, File::A),
+    }
+}
+
+const fn castling_rook_to_square(flank: Flank, color: Color) -> Square {
+    let rank = Rank::One.from_perspective(color);
+    match flank {
+        Flank::Kingside => Square::from_rank_and_file(rank, File::F),
+        Flank::Queenside => Square::from_rank_and_file(rank, File::D),
     }
 }
