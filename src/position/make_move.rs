24f8df495c5e0f306ae4849e10e253a10825d4a6
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2026-04-27 22:43:28 -0700

    Move unmake_move.rs contents to make_move.rs

--- src/position/make_move.rs
+++ src/position/make_move.rs
@@ -1,4 +1,4 @@
-//! Contains [`Position::make_move`].
+//! Contains [`Position::make_move`] and [`Position::unmake_move`].
 
 use crate::Color;
 use crate::ColoredPiece;
@@ -6,7 +6,7 @@ use crate::Flank;
 use crate::Piece;
 use crate::Square;
 use crate::r#move::{Move, MoveType};
-use crate::position::Position;
+use crate::position::{GameResult, Position};
 use crate::position::context::PositionContext;
 use crate::{ConstDoublePawnPushFile, DoublePawnPushFile};
 
@@ -90,6 +90,64 @@ impl<const N: usize, const STM: Color> Position<N, STM> {
 
         self.halfmove += 1;
         self.push_context(new_context);
+        self.update_pins_and_checks_for_stm(STM.other());
+    }
+
+    /// Undoes a move in place. After this, memory matches the destination type of [`Position::unmake_move`].
+    pub fn unmake_move(&mut self, mv: Move) {
+        let src_square = mv.source();
+        let dst_square = mv.destination();
+        let move_type = mv.move_type();
+        let mover = STM.other();
+
+        match move_type {
+            MoveType::Castling => {
+                debug_assert_eq!(self.context().captured_piece, Piece::Null);
+                self.board.apply_move::<false>(
+                    dst_square,
+                    src_square,
+                    mover,
+                    Piece::King,
+                    Piece::Null,
+                    move_type,
+                );
+            }
+            MoveType::EnPassant => {
+                debug_assert_eq!(self.context().captured_piece, Piece::Pawn);
+                self.board.apply_move::<false>(
+                    dst_square,
+                    src_square,
+                    mover,
+                    Piece::Pawn,
+                    Piece::Null,
+                    move_type,
+                );
+            }
+            MoveType::Normal | MoveType::NormalCapture => {
+                self.board.apply_move::<false>(
+                    dst_square,
+                    src_square,
+                    mover,
+                    self.board.piece_at(dst_square),
+                    self.context().captured_piece,
+                    move_type,
+                );
+            }
+            _ => {
+                self.board.apply_move::<false>(
+                    dst_square,
+                    src_square,
+                    mover,
+                    Piece::Pawn,
+                    self.context().captured_piece,
+                    move_type,
+                );
+            }
+        }
+
+        self.halfmove -= 1;
+        self.decrement_context_stack_for_unmake();
+        self.result = GameResult::None;
         self.update_pins_and_checks_for_stm(STM.other());
     }
 }
