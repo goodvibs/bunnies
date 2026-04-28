55831229631907e60064a7e0707ef748f2d5c9c3
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2026-04-27 22:19:49 -0700

    Perf

--- src/position/unmake_move.rs
+++ src/position/unmake_move.rs
@@ -12,47 +12,21 @@ impl<const N: usize, const STM: Color> Position<N, STM> {
         let dst_square = mv.destination();
         let move_type = mv.move_type();
         let mover = STM.other();
-        let captured_piece = self.context().captured_piece;
 
         match move_type {
-            MoveType::Normal | MoveType::DoublePawnPush => {
-                let moved_piece = self.board.piece_at(dst_square);
-                debug_assert_ne!(moved_piece, Piece::Null);
-                self.board.apply_move::<false>(
-                    dst_square,
-                    src_square,
-                    mover,
-                    moved_piece,
-                    captured_piece,
-                    move_type,
-                );
-            }
-            MoveType::NormalCapture => {
-                let moved_piece = self.board.piece_at(dst_square);
-                debug_assert_ne!(moved_piece, Piece::Null);
-                debug_assert_ne!(captured_piece, Piece::Null);
-                debug_assert_ne!(captured_piece, Piece::King);
-                self.board.apply_move::<false>(
-                    dst_square,
-                    src_square,
-                    mover,
-                    moved_piece,
-                    captured_piece,
-                    move_type,
-                );
-            }
             MoveType::Castling => {
+                debug_assert_eq!(self.context().captured_piece, Piece::Null);
                 self.board.apply_move::<false>(
                     dst_square,
                     src_square,
                     mover,
                     Piece::King,
-                    captured_piece,
+                    Piece::Null,
                     move_type,
                 );
             }
             MoveType::EnPassant => {
-                debug_assert_eq!(captured_piece, Piece::Pawn);
+                debug_assert_eq!(self.context().captured_piece, Piece::Pawn);
                 self.board.apply_move::<false>(
                     dst_square,
                     src_square,
@@ -62,38 +36,25 @@ impl<const N: usize, const STM: Color> Position<N, STM> {
                     move_type,
                 );
             }
-            MoveType::PushPromotionToKnight
-            | MoveType::PushPromotionToBishop
-            | MoveType::PushPromotionToRook
-            | MoveType::PushPromotionToQueen
-            | MoveType::CapturePromotionToKnight
-            | MoveType::CapturePromotionToBishop
-            | MoveType::CapturePromotionToRook
-            | MoveType::CapturePromotionToQueen => {
-                let promotion_piece = move_type.promotion_piece();
-                debug_assert_ne!(promotion_piece, Piece::Null);
-                if move_type.is_capture() {
-                    debug_assert_ne!(captured_piece, Piece::Null);
-                    debug_assert_ne!(captured_piece, Piece::King);
-                    self.board.apply_move::<false>(
-                        dst_square,
-                        src_square,
-                        mover,
-                        Piece::Pawn,
-                        captured_piece,
-                        move_type,
-                    );
-                } else {
-                    debug_assert_eq!(captured_piece, Piece::Null);
-                    self.board.apply_move::<false>(
-                        dst_square,
-                        src_square,
-                        mover,
-                        Piece::Pawn,
-                        captured_piece,
-                        move_type,
-                    );
-                }
+            MoveType::Normal | MoveType::NormalCapture => {
+                self.board.apply_move::<false>(
+                    dst_square,
+                    src_square,
+                    mover,
+                    self.board.piece_at(dst_square),
+                    self.context().captured_piece,
+                    move_type,
+                );
+            }
+            _ => {
+                self.board.apply_move::<false>(
+                    dst_square,
+                    src_square,
+                    mover,
+                    Piece::Pawn,
+                    self.context().captured_piece,
+                    move_type,
+                );
             }
         }
 
