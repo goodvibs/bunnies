606b592795031c40ad1b3ea830cf4dc66d2528df
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2026-05-05 23:02:28 -0700

    Remove unused

--- crates/bunnies/src/legal_gen_kind.rs
+++
@@ -1,7 +0,0 @@
-/// Which subset of legal moves to generate (split for quiescence search and ordering).
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum LegalGenKind {
-    All,
-    Captures,
-    Quiets,
-}
